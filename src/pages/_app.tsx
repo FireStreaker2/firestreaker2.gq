4c6685d821d0859d69e32e3323439b09685a700c
FireStreaker2 <michaelccc2019@outlook.com>
2024-06-27 11:55:07 -0700

    nextui and tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
+import { NextUIProvider } from "@nextui-org/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+	return (
+		<NextUIProvider>
+			<main className="light text-foreground bg-background">
+				<Component {...pageProps} />
+			</main>
+		</NextUIProvider>
+	);
 }
