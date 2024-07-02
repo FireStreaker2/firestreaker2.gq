396f1ca4d956f523dbf8697bd2336e4c589e2ade
FireStreaker2 <michaelccc2019@outlook.com>
2024-07-01 22:23:10 -0700

    small optimizations

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+export default function NotFound() {
+	const router = useRouter();
+
+	useEffect(() => {
+		router.replace("/");
+	});
+};
